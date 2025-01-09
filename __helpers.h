384894c578338574a0b660a14f17d0d25ff61d34
Raj <146608157+RajDevX@users.noreply.github.com>
2025-01-09 08:14:46 +0530

    Nuitka Python Function Call Prototypes Header
    This header defines Nuitka functions for calling Python functions with 12, 14, or 17 arguments, optimizing performance and thread safety

---
+++ __helpers.h
@@ -0,0 +1,7 @@
+#ifndef __NUITKA_CALLS_H__
+#define __NUITKA_CALLS_H__
+
+extern PyObject *CALL_FUNCTION_WITH_ARGS12(PyThreadState *tstate, PyObject *called, PyObject *const *args) ;
+extern PyObject *CALL_FUNCTION_WITH_ARGS14(PyThreadState *tstate, PyObject *called, PyObject *const *args) ;
+extern PyObject *CALL_FUNCTION_WITH_ARGS17(PyThreadState *tstate, PyObject *called, PyObject *const *args) ;
+#endif
